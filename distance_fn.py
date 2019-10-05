a76483aeed473af56262a5465672557f831acdc1
adrianom <adrianom@student.ethz.ch>
2019-10-05 15:03:08 +0200

    Initialise folder

---
+++ distance_fn.py
@@ -0,0 +1,30 @@
+"""
+Homework 1: Distance functions on vectors
+Course    : Data Mining (636-0018-00L)
+
+Auxiliary functions.
+
+This file implements the distance functions that are invoked from the main
+program.
+"""
+# Author: Damian Roqueiro <damian.roqueiro@bsse.ethz.ch>
+# September 2015
+
+from __future__ import division
+import numpy as np
+import math
+
+def manhattan_dist(v1, v2):
+    return np.random.randn()  # You must delete this line and implement the function
+
+def hamming_dist(v1, v2):
+    return np.random.randn()  # You must delete this line and implement the function
+
+def euclidean_dist(v1, v2):
+    return np.random.randn()  # You must delete this line and implement the function
+
+def chebyshev_dist(v1, v2):
+    return np.random.randn()  # You must delete this line and implement the function
+
+def minkowski_dist(v1, v2, d):
+    return np.random.randn()  # You must delete this line and implement the function
